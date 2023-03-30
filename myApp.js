b461aab7781150f103f1f778adde8ae5811c4b34
AshlavaDev <104705344+AshlavaDev@users.noreply.github.com>
2023-03-30 12:59:39 -0600

    exercise 2, added schema

--- myApp.js
+++ myApp.js
@@ -3,8 +3,19 @@ const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
+const personSchema = new mongoose.Schema({
+  name: {
+    type: String,
+  },
+  age: {
+    type: Number
+  },
+  favoriteFoods: {
+    type: [String]
+  }
+});
 
-let Person;
+let Person = new mongoose.model('Person', personSchema);
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
