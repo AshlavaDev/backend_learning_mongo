a75a9b6c96a2ef84ca5318dcc2c62bfe3f659665
AshlavaDev <104705344+AshlavaDev@users.noreply.github.com>
2023-03-30 18:50:40 -0600

    exercise 7, find a person byt their unique id

--- myApp.js
+++ myApp.js
@@ -70,8 +70,15 @@ const findOneByFood = (food, done) => {
   })
 };
 
+let personId = 1;
+
 const findPersonById = (personId, done) => {
-  done(null /*, data*/);
+
+  Person.findById({_id: personId}, (err, data) => {
+    if(err) return console.error(err);
+
+    done(null, data);
+  })
 };
 
 const findEditThenSave = (personId, done) => {
